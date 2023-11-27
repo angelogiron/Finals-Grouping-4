6f21f12a4479b577a5e1ec768e9a7789310c9a8c
DESKTOP-QKRH6T4\justi <justingabrielle.lipana@uap.asia>
2023-11-27 17:42:45 +0800

    Fixing logic for logging in process.

--- BackEnd/oneflight-app/src/main/java/com/it1311l/uap/oneflightapp/controller/AccountsController.java
+++ BackEnd/oneflight-app/src/main/java/com/it1311l/uap/oneflightapp/controller/AccountsController.java
@@ -4,6 +4,7 @@ import com.it1311l.uap.oneflightapp.model.Account;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +20,20 @@ public class AccountsController
 	{
 		accountsRepository.insert(account);
 		return account;
+	}
+	
+	@PostMapping("/login")
+	public String login(@RequestBody Account loginRequest) {
+	    String email = loginRequest.getAccountEmail();
+	    String password = loginRequest.getAccountPassword();
+
+	    Account storedAccount = accountsRepository.findByEmail(email);
+	    
+	    
+	    if (storedAccount != null && password.equals(storedAccount.getAccountPassword())) {
+	        return "Login successful!";
+	    } else {
+	        return "Login failed. Invalid email or password.";
+	    }
 	}
 }
