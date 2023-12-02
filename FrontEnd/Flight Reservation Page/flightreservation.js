93549c4abe77d3bf1b7b7879c0a3eba5b6642853
DESKTOP-QKRH6T4\justi <justingabrielle.lipana@uap.asia>
2023-12-03 00:16:49 +0800

    Fixed the redirect button na hindi magawa ni roice hahah!! BASIC!

--- FrontEnd/Flight Reservation Page/flightreservation.js
+++ FrontEnd/Flight Reservation Page/flightreservation.js
@@ -1,6 +1,12 @@
 document.getElementById("book-flight-btn").addEventListener("click", login);
 document.getElementById("cancel-btn").addEventListener("click", goToHomePage);
 
-async function goToHomePage() {
-    window.location.href = "../../Home Page/homepagedump.html";
+async function goToHomePage() 
+{
+    window.location.href = "../Home Page/homepagedump.html";
+}
+
+async function login()
+{
+
 }
