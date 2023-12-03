cf2a416ea0f72bae4c16f484da03015d07d5082f
DESKTOP-FOBIULO\LaptopAlwen <alwyn.macabugwas@uap.asia>
2023-12-03 20:58:41 +0800

    onting fix lang

--- FrontEnd/Maintenance Pages/useradministrationpage/useradministrationpage.js
+++ FrontEnd/Maintenance Pages/useradministrationpage/useradministrationpage.js
@@ -12,6 +12,7 @@ async function getEmails(){
         const response = await fetch(url, options);
         const result = await response.json();
         console.log(result);
+        return result;
     }
     catch (error) {
         console.error(error);
